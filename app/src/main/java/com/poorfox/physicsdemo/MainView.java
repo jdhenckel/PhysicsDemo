788eb096e37ba1d69b9780168ebfd917d7b8a836
John Henckel <Henckel.Jonathan@mayo.edu>
2021-02-15 23:07:05 -0600

    better button

--- app/src/main/java/com/poorfox/physicsdemo/MainView.java
+++ app/src/main/java/com/poorfox/physicsdemo/MainView.java
@@ -2,6 +2,7 @@ package com.poorfox.physicsdemo;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.Matrix;
 import android.graphics.Paint;
 import android.hardware.Sensor;
@@ -79,13 +80,14 @@ public class MainView extends View
 
         // Scale the camera to width 10, with origin in LOWER left
         float scale = height / 10;   // pixels per meter
-        //cameraMatrix.setTranslate(0, -height /scale);
         cameraMatrix.setRotate(90);
         cameraMatrix.preScale(scale, -scale);
-        //cameraMatrix.preTranslate(0, -height /scale);
 
+        // Translate the device origin to the TOP left
+        //deviceMatrix.setTranslate(0, 500);
+//        deviceMatrix.preRotate(90);
         deviceMatrix.setRotate(90);
-        //deviceMatrix.preScale(height / 100, height / 100);    // TODO -- remove this
+        deviceMatrix.preTranslate(0, -width);
         startWorldSimulation();
     }
 
@@ -180,21 +182,58 @@ public class MainView extends View
     {
         Paint paint = new Paint();
         paint.setColor(0xFF80FF80);
-        for (int i = 0; i < height; i += 100)
+        for (int i = 0; i < height; i += 200)
         {
-            canvas.drawLine(0, -i, 100, -i, paint);
-            canvas.drawLine(i, -height, i, 0, paint);
+            canvas.drawLine(0, i, height, i, paint);
+            canvas.drawLine(i, 0, i, width, paint);
         }
         paint.setColor(0xffff8080);
-        canvas.drawCircle(width-40,40-height,35,paint);
-        paint.setColor(0xFF000000);
-        paint.setTextSize(25);
-        canvas.drawText("EDIT", width-40, 40, paint);
+        canvas.drawCircle(400,400,100,paint);
+
+        //paint.setColor(0xFF000000);
+        //paint.setTextSize(70);
+        //canvas.drawText("EDIT", 400-50,400+20, paint);
+
+        Widget edit = new Widget(19,19,"EDIT");
+        edit.onDraw(canvas);
+
     }
 
     class Widget {
-        int x,y;
+        int x,y,w,h;
         String label;
+        Paint paint;
+
+        Widget(int x, int y, String label)
+        {
+            this.x = x; this.y = y; this.label = label;
+            w = h = 200;
+            paint = new Paint();
+        }
+
+        void onDraw(Canvas canvas)
+        {
+            paint.setColor(Color.rgb(173, 177, 179));
+            //canvas.drawRoundRect(-10,-10,290,290,29,29,paint);
+            drawRect(canvas, 0, 0, -50, -50, 50);
+            paint.setColor(Color.rgb(10,10,10));
+            drawRect(canvas, 20, 20);
+            paint.setColor(Color.rgb(63, 84, 89));
+            drawRect(canvas, 0, 0);
+            paint.setColor(Color.rgb(213, 221, 224));
+            drawRect(canvas, 0, 0, 20, 20, 20);
+            int s = Math.min(h/2, w / label.length());
+            paint.setTextSize(s*1.3f);
+            paint.setColor(Color.rgb(52, 57, 59));
+            canvas.drawText(label, x + 0.13f * w, y + .5f * (h + s), paint);
+        }
+
+        void drawRect(Canvas canvas,int tx,int ty) { drawRect(canvas, tx, ty, 0, 0, 20); }
+
+        void drawRect(Canvas canvas,int tx,int ty,int ex, int ey, int r)
+        {
+            canvas.drawRoundRect(x+tx+ex,y+ty+ey,tx-ex+x+w,ty-ey+y+h,r,r,paint);
+        }
 
     }
 
